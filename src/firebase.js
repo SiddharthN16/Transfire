9f36311921bc776d6d3eaa857a1a9cd10c99b293
Siddharth <siddharthnema04@gmail.com>
2022-08-29 22:50:56 -0400

    switched to firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // const firebaseConfig = {
@@ -24,6 +24,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 export default app;
